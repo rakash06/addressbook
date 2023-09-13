6c5790248cab82763c2325ffd23fbcbc7698921c
Akash R <rakashofficial06@gmail.com>
2023-09-13 23:01:23 +0530

    updated11

--- server-config.sh
+++ server-config.sh
@@ -3,14 +3,14 @@ sudo yum install java-1.8.0-openjdk-devel
 sudo yum install git -y
 sudo yum install maven -y
 
-if [ -d "addressbookpractice" ]
+if [ -d "addressbook" ]
 then
     echo "Repository is already cloned and exist"
-    cd /home/ec2-user/addressbookpractice
+    cd /home/ec2-user/addressbook
     git pull origin demo-practice
 else
 git clone https://github.com/rakash06/addressbook.git
 fi
 
-cd /home/ec2-user/addressbookpractice
+cd /home/ec2-user/addressbook
 mvn package
